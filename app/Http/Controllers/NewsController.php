8e6038f90e99348cb1c7d10d73587423de889bd0
Lana <lana.bochkova2015@yandex.ru>
2021-06-29 00:04:40 +0300

    Создание категории и валидация request
    При успешной попытке добавления новости происходит redirect на главную страницу новостей с flash-сообщением об успешном добавлении

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\News\StoreNewsRequest;
+use App\Http\Requests\NewsShowRequest;
+use App\Models\Category;
 use App\Models\News;
 
 class NewsController extends Controller
 {
     public function index()
     {
-
         $news = News::with('category')->get();
         // dd($news);
         return view('news.index', compact('news'));
@@ -16,6 +18,22 @@ class NewsController extends Controller
 
     public function show(News $news)
     {
-        return view('news.show', compact('news'));
+        return response()
+            ->view('news.show', compact('news'))
+            ->header('x-app-type', 'news-page');
+            // return view('news.show', compact('news'));
+    }
+
+    public function create()
+    {
+        $categories = Category::all();
+        return view('news.create', compact('categories'));
+    }
+
+    public function store(StoreNewsRequest $request)
+    {
+        News::create($request->validated());
+
+        return redirect()->route('news.index')->with('success', 'Новость успешно добавлена');
     }
 }
