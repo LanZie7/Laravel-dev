f364acb55ae11917f99ce9240331847b7d528341
Lana <lana.bochkova2015@yandex.ru>
2021-07-16 18:07:27 +0300

    Список новостей в админке
    Временный. Будет изменен. Скорее всего под Tailwind

---
+++ resources/views/admin/news/index.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts/admin/index')
+@section('title') Список новостей - @parent @stop
+@section('content')
+
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Новости</h1>
+            <a href="{{ route('admin.news.create') }}" class="btn btn-primary" style="float: right">Добавить новость</a>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item active">Список новостей</li>
+            </ol>
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    Список новостей
+                </div>
+                <div class="card-body">
+                    <table id="datatablesSimple">
+                        <thead>
+                            <tr>
+                                <th>#ID</th>
+                                <th>Заголовок</th>
+                                <th>Категория</th>
+                                <th>Описание</th>
+                                <th>Дата добавления</th>
+                                <th>Управление</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($newsList as $news)
+                                <tr>
+                                    <td>{{ $news->id }}</td>
+                                    <td>{{ $news->title }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.categories.filter', ['id' => $news->category_id]) }}">
+                                            {{ $news->categoryTitle }}
+                                        </a>
+                                    </td>
+                                    <td>{{ $news->description }}</td>
+                                    <td>{{ $news->created_at }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.news.edit', ['news' => $news->id]) }}" style="font-size: 12px;">Ред.</a> &nbsp; | &nbsp;
+                                        <a href="javascript:;" style="font-size: 12px; color: red;">Уд.</a></td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="5">Новостей не найдено</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
+
+@endsection
